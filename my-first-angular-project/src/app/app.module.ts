a0b99824373e6399691397f08173394ce419f101
Nicolas GAUTIER <nicolas.gautier35135@gmail.com>
2023-08-14 14:08:34 +0200

    add currency french format

--- my-first-angular-project/src/app/app.module.ts
+++ my-first-angular-project/src/app/app.module.ts
@@ -3,7 +3,11 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { SalesPersonListComponent } from './sales-person-list/sales-person-list.component';
+import { LOCALE_ID } from '@angular/core';
+import { registerLocaleData } from '@angular/common';
+import localeFr from '@angular/common/locales/fr'
 
+registerLocaleData(localeFr, 'fr');
 @NgModule({
   declarations: [
     AppComponent,
